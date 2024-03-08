a86533027d6195f585933ae7c03e58e15d699e24
Tatiana Chepurnaya <tanyusha_tokor@mail.ru>
2024-03-09 04:31:27 +0500

    correcting code vacancy.py

--- code/vacancy.py
+++ code/vacancy.py
@@ -1,38 +1,63 @@
+import json
+
+
 class Vacancy:
     """
     Класс для работы с вакансией
     """
-    def __init__(self, name: str, url: str, salary: dict, area: str, published_at: str):
+
+    def __init__(self, name: str, url: str, salary_from: int, salary_to: int, area: str, published_at: str):
         """
         :param name: название вакансии
         :param url: ссылка на вакансию
-        :param salary: предлагаемая зарплата
-        :param area: описании вакансии
+        :param salary_from: предлагаемая зарплата от
+        :param salary_to: предлагаемая зарплата
+        :param area: Регион
         :published_at: дата публикации
         """
         self.name = name
         self.url = url
-        try:
-            if not salary:
-                raise TypeError
-        except TypeError:
-            self.__salary = 0
-        self.__salary = salary
+        self.__salary_from = salary_from
+        self.__salary_to = salary_to
         self.area = area
         self.published_at = published_at
 
     @classmethod
-    def new_vacancy(cls, vacancy: dict):
+    def cast_to_object_list(cls, vacancies_json: json):
         """
-        :param vacancy: словарь с новой вакансией
-        :return: объект класса Vacancy
+        :param vacancies_json: информация о вакансиях в формате JSON
+        :return: список объектов вакансий
         """
-        return cls(**vacancy)
+        with open(vacancies_json, encoding='utf-8') as json_file:
+            data_list = json.load(json_file)
+            vacancies_list = []
+            for vacancy in data_list:
+                try:
+                    name, url, salary_from, salary_to, area, published_at = (vacancy['name'],
+                                                                             vacancy['url'],
+                                                                             vacancy['salary']['from'],
+                                                                             vacancy['salary']['to'],
+                                                                             vacancy['area']['name'],
+                                                                             vacancy['published_at'])
+                except KeyError:
+                    salary_from, salary_to = 0, 0
+                vacancies_list.append(cls(name, url, salary_from, salary_to, area, published_at))
+            return vacancies_list
 
-    def __le__(self, other):
-        return self.__salary <= other.salary
+    def __le__(self, salary_to: int):
+        """
+        :param salary_to: зарплата, предлагаемая пользователем до
+        :return: True, если self.__salary_to <= salary_to
+        """
+        if not isinstance(salary_to, int):
+            raise TypeError
+        return self.__salary_to <= salary_to
 
-    def __ge__(self, other):
-        return self.__salary >= other.salary
-
-
+    def __ge__(self, salary_from: int):
+        """
+        :param salary_from: зарплата, предлагаемая пользователем от
+        :return: True, если self.__salary_from >= salary_from
+        """
+        if not isinstance(salary_from, int):
+            raise TypeError
+        return self.__salary_from >= salary_from
