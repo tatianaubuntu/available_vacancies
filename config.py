066f8c38823eeb618e5fccfc593329a83080a575
Tatiana Chepurnaya <tanyusha_tokor@mail.ru>
2024-03-10 10:44:20 +0500

    add JSON file

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ from os import path
 
 parent_dir = path.dirname(path.abspath(__file__))
 VACANCIES_FILE = path.join(parent_dir, 'data', 'vacancies.json')
+TEST_VACANCIES_FILE = path.join(parent_dir, 'tests', 'test_vacancies.json')
